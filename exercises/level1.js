26872db8a26162deabb885b344ba69be9418be4f
Robert Martinez <robertmartinez@Roberts-MBP-2.localdomain>
2018-09-25 19:12:02 -0500

    saving my exercises

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,43 @@
 function lvl1exercise1 () {
   // Declare a variable without instantiating it and return it!
-
+  var1 = 4
 }
 
 function lvl1exercise2 () {
   // Declare and instantiate a number and return it
-
+  var answer2 = 4
+  return answer2
 }
 
 function lvl1exercise3 () {
   // Declare and instantiate a floating point number that is not a whole number and return it
-
+  var answer3 = 3.14
+  return answer3
 }
 
 function lvl1exercise4 () {
   // Declare and instantiate a string "Hello World!" and return it
-
+  var string1 = "Hello World!";
+  return string1
 }
 
 function lvl1exercise5 () {
   // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
-
+var arr1 = [("Hello World!"), 4]
+return arr1
 }
 
 function lvl1exercise6 () {
   // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+var ob1 = {
+    key1 : "Hello World!",
+    key2 : 4
+}
+return ob1
 }
 
 function lvl1exercise7 () {
   // Declare and instantiate a boolean value 'false' and return it
-
+var boolean = false
+return boolean
 }
--- exercises/level7.js
+++ exercises/level7.js
@@ -1,5 +1,9 @@
-// Write a function that takes a number as an input.
+// Write a function that takes a number as an input
 // Have it create an empty array and then push a string into the array as many
 // times as the input number
 //
 // Name the function "finalFunction"
+function finalFunction(){
+    arr.push('hello')
+    return finalFunction 
+}
